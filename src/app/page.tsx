6a593df0c6913a9e1788c528a268b83dff4b0135
Ryan Shee <ryan.shee@gmail.com>
2024-01-02 02:12:54 -0800

    Add ratings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
         <LocationPicker />
         <RangePicker />
         <PricePicker />
-        {/* <RatingPicker /> */}
+        <RatingPicker />
         {/* <AttributePicker /> */}
       </div>
     </main>
--- src/app/ratingPicker/RatingPicker.tsx
+++ src/app/ratingPicker/RatingPicker.tsx
@@ -1,7 +1,26 @@
+import { Rating } from "@mui/material";
+import styles from "./RatingPicker.module.css";
+import { useState } from "react";
+
 export default function RatingPicker() {
+  const [rating, setRating] = useState(0);
+
+  const ratingPicked = (event: any, value: any) => {
+    setRating(value);
+  };
+
   return (
-    <div>
-      <h1>5. How low is the bar?</h1>
-    </div>
+    <section className={styles.container}>
+      <h1>5. Set minimum rating.</h1>
+      <div className={styles.ratingContainer}>
+        <Rating
+          defaultValue={0}
+          precision={0.5}
+          className={styles.ratings}
+          onChange={ratingPicked}
+          size="large"
+        />
+      </div>
+    </section>
   );
 }
