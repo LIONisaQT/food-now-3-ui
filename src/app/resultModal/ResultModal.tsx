3661a72319c7e53277378f89b26e74c365ba05d5
Ryan Shee <ryan.shee@gmail.com>
2024-01-07 17:33:25 -0800

    Fix rating precision, fix some React SVG complains

--- src/app/resultModal/ResultModal.tsx
+++ src/app/resultModal/ResultModal.tsx
@@ -65,7 +65,12 @@ export default function ResultModal({ result, closeCallback }: ResultProps) {
           </div>
           <section className={styles.modalBody}>
             <div className={styles.rating}>
-              <Rating readOnly value={result.rating} size="large" />
+              <Rating
+                readOnly
+                value={result.rating}
+                size="large"
+                precision={0.5}
+              />
               <p className={`${styles.ratingText} ${styles.bodyText}`}>
                 {result.review_count} review
                 {result.review_count === 1 ? "" : "s"}
@@ -104,13 +109,13 @@ export default function ResultModal({ result, closeCallback }: ResultProps) {
                     height={32}
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke-width="1.5"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     className="w-6 h-6"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z"
                     />
                   </svg>
@@ -124,7 +129,7 @@ export default function ResultModal({ result, closeCallback }: ResultProps) {
                     height={32}
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke-width="1.5"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     className="w-6 h-6"
                   >
